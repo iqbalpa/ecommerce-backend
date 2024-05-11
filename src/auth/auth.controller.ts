38a5f88e6f0f50b08bd8e71e2f6387870808dbf8
iqbalpa <iqbalpahlevi16@gmail.com>
2024-05-11 15:53:05 +0700

    feat(auth): create input validator middleware

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,44 +7,63 @@ import handler from "../utils/handler";
 
 const authRouter = Router();
 
-authRouter.post("/register", async (req: Request, res: Response) => {
-	const userData: UserRequest = {
-		email: req.body.email,
-		name: req.body.name,
-		password: req.body.password,
-	};
-	const user: UserResponse = await authService.registerUser(userData);
-	handler.successHandler({ message: "user registered", data: user, status: 201 }, res);
-});
-
-authRouter.post("/login", async (req: Request, res: Response) => {
-	const email: string = req.body.email;
-	const password: string = req.body.password;
-	const data: { ur: UserResponse; token: string } | string = await authService.loginUser(email, password);
-	if (typeof data === "string") {
-		handler.errorHandler({ message: data, data: data, status: 404 }, res);
-	} else {
-		handler.successHandler(
-			{ message: "login success", data: { user: data.ur, token: data.token }, status: 200 },
-			res
-		);
+authRouter.post(
+	"/register",
+	authMiddleware.isEmailValid,
+	authMiddleware.isPasswordValid,
+	authMiddleware.isNameValid,
+	async (req: Request, res: Response) => {
+		const userData: UserRequest = {
+			email: req.body.email,
+			name: req.body.name,
+			password: req.body.password,
+		};
+		const user: UserResponse = await authService.registerUser(userData);
+		handler.successHandler({ message: "user registered", data: user, status: 201 }, res);
 	}
-});
+);
 
-authRouter.get("/user-detail", authMiddleware.userAuth, async (req: Request, res: Response) => {
-	const email: string = req.user.email;
-	const user: UserResponse | string = await authService.getUserDetail(email);
-	if (typeof user === "string") {
-		handler.errorHandler({ message: user, data: user, status: 404 }, res);
-	} else {
-		handler.successHandler({ message: "user detail retrieved", data: user, status: 200 }, res);
+authRouter.post(
+	"/login",
+	authMiddleware.isEmailValid,
+	authMiddleware.isPasswordValid,
+	async (req: Request, res: Response) => {
+		const email: string = req.body.email;
+		const password: string = req.body.password;
+		const data: { ur: UserResponse; token: string } | string = await authService.loginUser(email, password);
+		if (typeof data === "string") {
+			handler.errorHandler({ message: data, data: data, status: 404 }, res);
+		} else {
+			handler.successHandler(
+				{ message: "login success", data: { user: data.ur, token: data.token }, status: 200 },
+				res
+			);
+		}
 	}
-});
+);
+
+authRouter.get(
+	"/user-detail",
+	authMiddleware.userAuth,
+	authMiddleware.isEmailValid,
+	async (req: Request, res: Response) => {
+		const email: string = req.user.email;
+		const user: UserResponse | string = await authService.getUserDetail(email);
+		if (typeof user === "string") {
+			handler.errorHandler({ message: user, data: user, status: 404 }, res);
+		} else {
+			handler.successHandler({ message: "user detail retrieved", data: user, status: 200 }, res);
+		}
+	}
+);
 
 authRouter.put(
 	"/update",
 	authMiddleware.userAuth,
 	authMiddleware.manageAccountAuth,
+	authMiddleware.isEmailValid,
+	authMiddleware.isPasswordValid,
+	authMiddleware.isNameValid,
 	async (req: Request, res: Response) => {
 		const email: string = req.body.email;
 		const userData: UserRequest = {
@@ -61,6 +80,9 @@ authRouter.delete(
 	"/delete",
 	authMiddleware.userAuth,
 	authMiddleware.manageAccountAuth,
+	authMiddleware.isEmailValid,
+	authMiddleware.isPasswordValid,
+	authMiddleware.isNameValid,
 	async (req: Request, res: Response) => {
 		const email: string = req.body.email;
 		const deletedUser: UserResponse = await authService.deleteUser(email);
@@ -68,13 +90,18 @@ authRouter.delete(
 	}
 );
 
-authRouter.post("/register-admin", async (req: Request, res: Response) => {
-	const adminData: AdminRequest = {
-		email: req.body.email,
-		password: req.body.password,
-	};
-	const admin: AdminResponse = await authService.registerAdmin(adminData);
-	handler.successHandler({ message: "admin registered", data: admin, status: 201 }, res);
-});
+authRouter.post(
+	"/register-admin",
+	authMiddleware.isEmailValid,
+	authMiddleware.isPasswordValid,
+	async (req: Request, res: Response) => {
+		const adminData: AdminRequest = {
+			email: req.body.email,
+			password: req.body.password,
+		};
+		const admin: AdminResponse = await authService.registerAdmin(adminData);
+		handler.successHandler({ message: "admin registered", data: admin, status: 201 }, res);
+	}
+);
 
 export default authRouter;
